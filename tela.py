e5c9fcdf8d53916bb507f80f6d9baecf821c8231
bacr <dev.breno07@gmail.com>
2026-06-09 12:00:28 -0300

    criacao da tela

---
+++ tela.py
@@ -0,0 +1,21 @@
+import pygame 
+from pygame.locals import*
+from sys import exit
+
+pygame.init
+
+largura = 2040
+altura = 1400
+
+
+tela = pygame.display.set_mode((largura, altura))
+pygame.display.set_caption("PC-Hersteller")
+
+while True:
+    tela.fill((0, 0, 0))
+    for event in pygame.event.get():
+        if event.type  == QUIT:
+            pygame.quit()
+            exit()
+
+    pygame.display.update()                                 
