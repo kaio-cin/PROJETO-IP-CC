fd58982ed957759c0c3908a9a3d67336ac86fd29
Nina Cantilino <ninacantilino@gmail.com>
2026-06-14 14:22:09 -0300

    criacao das portas logicas e começando a lógica do tempo do jogo

--- tela.py
+++ tela.py
@@ -21,12 +21,26 @@ barulho_fechei.set_volume(1)
 largura = 1550
 altura = 800
 
+#nivel 1 - portas logicas
+qtd_portas = 0
+meta_portas = 3
+
+#nivel 2 - mux e demux
+qtd_combinacional = 0
+meta_combinacional = 3
+
+#nivel 3 - contador, registrador e flip-flop
+qtd_sequencial = 0
+meta_sequencial = 5
 
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("PC-Hersteller")
 
 clock = pygame.time.Clock()
 fonte = pygame.font.SysFont('Arial', 20, True) #tamanho aleatório de fonte, quando a gente testar a gente troca
+
+tempo_inicio = pygame.time.get_ticks()  #pega o tempo do inicio do jogo
+limite = 120 * 1000  #2 minutos em milissegundos
 
 inventario = {"AND": 0, "NAND": 0, "NOT": 0, "OR": 0, "MUX": 0, "DMUX": 0, "Contador": 0, "Registrador": 0, "Flipflop": 0}
 
@@ -51,7 +65,6 @@ class Personagem():
             self.rect.y -= self.vel
         if teclas[self.baixo]: 
             self.rect.y += self.vel
-
 
     def desenhar(self, surf):
         surf.blit(self.sprite, self.rect)
@@ -123,6 +136,19 @@ Stefan = Personagem(
 while True:
     clock.tick(60)
     tela.fill((30, 30, 40))
+    
+    tempo_passado = pygame.time.get_ticks() - tempo_inicio
+    tempo_restante = max(0, limite - tempo_passado) #o max evita tempo negativo
+    
+    segundos_restantes = tempo_restante // 1000
+
+    minutos = segundos_restantes // 60
+    segundos = segundos_restantes % 60
+    texto_tempo = f'{minutos:02d}:{segundos:02d}'
+
+    texto_surface = fonte.render(texto_tempo, True, (255, 255, 255))
+    tela.blit(texto_surface, (10, 10))  #posição x=10, y=10 (canto superior esquerdo)
+
     for event in pygame.event.get():
         if event.type  == QUIT:
             pygame.quit()
